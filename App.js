150e1365a7703578c5fbfc3137e73ec1ea1ef544
nguye374 <nguye374@sheridancollege.ca>
2023-02-19 22:00:16 -0500

    update dark background

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Text } from 'react-native';
 import ReactThreeFiber from './src/Components/threefibernative';
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <ReactThreeFiber />
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      <Text style={{ color: '#fff' }}>Test</Text>
+      <ReactThreeFiber />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexGrow: 1,
+    backgroundColor: '#000000',
   },
 });
